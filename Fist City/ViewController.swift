83615000aec2b67b08a01bfde9cee27bb0bfbcf9
stone <stone.thirty@icloud.com>
2019-09-06 09:53:47 +0800

    初始功能

--- Fist City/ViewController.swift
+++ Fist City/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pr(p: "他有那鼻子也有那眉毛眼睛不会眨")
+    }
+    func pr(p:String){
+        print(p)
     }
 
 
